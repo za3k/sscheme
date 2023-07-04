881229d1af29d75809d6945205961c47d4d9dc78
Zachary Vance <za3k@za3k.com>
2023-07-03 21:52:53 -0400

    Improve error

--- types.c
+++ types.c
@@ -116,6 +116,6 @@ sexp* make_macro(sexp *function) {
 }
 
 sval* make_character_constant (char c) { 
-    if (((unsigned int)c) >= 128) return 0;
+    if (((unsigned int)c) >= 128) return error(ERR_INVALID_CHAR);
     return &CHARS_V[(int)c];
 };
